281a95ae1cdd2592e26969f6e06f049483407af4
David Schmitt <david.schmitt@puppet.com>
2020-03-26 09:04:00 +0000

    Initial commit of github_gem_owners gem scaffolding

---
+++ lib/github_gem_owners.rb
@@ -0,0 +1,6 @@
+require "github_gem_owners/version"
+
+module GithubGemOwners
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/github_gem_owners/version.rb
@@ -0,0 +1,3 @@
+module GithubGemOwners
+  VERSION = "0.1.0"
+end
